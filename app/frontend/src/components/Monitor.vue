3ed535d56a42bd1f7b2049ade216c3254454cef9
bronya0 <tangssst@qq.com>
2024-11-20 23:48:43 +0800

    add: color

--- app/frontend/src/components/Monitor.vue
+++ app/frontend/src/components/Monitor.vue
@@ -4,9 +4,11 @@
       <h2 style="max-width: 200px;">巡检</h2>
       <p>巡检Kafka积压情况。</p>
     </n-flex>
-
     <n-flex align="center">
-      必选：Topic，支持多选：
+      必须先选择一至多个topic，及group。每5分钟自动抓取一次数据
+    </n-flex>
+    <n-flex align="center">
+      Topic，支持多选：
       <n-select
           v-model:value="selectedTopics"
           :options="topic_data"
@@ -16,7 +18,7 @@
           multiple
           style="width: 600px"
       />
-      可选：Group
+      Group：
       <n-select
           v-model:value="selectedGroup"
           :options="group_data"
@@ -31,15 +33,9 @@
     </n-flex>
 
     <n-flex vertical>
-
       <n-flex align="center">
-      </n-flex>
-      <!-- 图表容器 -->
-      <div ref="commit_chartRef" style="width: 100%; height: 500px"></div>
-      <div ref="end_chartRef" style="width: 100%; height: 500px"></div>
-      <div ref="start_chartRef" style="width: 100%; height: 500px"></div>
-
-      <n-flex align="center">
+        <div ref="commit_chartRef" style="width: 48%; height: 500px"></div>
+        <div ref="end_chartRef" style="width: 48%; height: 500px"></div>
       </n-flex>
 
     </n-flex>
@@ -49,7 +45,7 @@
 </template>
 
 <script setup>
-import {onMounted, ref} from 'vue'
+import {onMounted, ref, shallowRef} from 'vue'
 import * as echarts from 'echarts/core';
 import {
   TitleComponent,
@@ -57,9 +53,9 @@ import {
   GridComponent,
   LegendComponent
 } from 'echarts/components';
-import { LineChart } from 'echarts/charts';
-import { UniversalTransition } from 'echarts/features';
-import { CanvasRenderer } from 'echarts/renderers';
+import {LineChart} from 'echarts/charts';
+import {UniversalTransition} from 'echarts/features';
+import {CanvasRenderer} from 'echarts/renderers';
 
 import {NButton, NFlex, useMessage} from "naive-ui";
 import {GetGroups, GetTopicOffsets, GetTopics} from "../../wailsjs/go/service/Service";
@@ -74,15 +70,12 @@ const group_data = ref([]);
 const selectedTopics = ref([])
 const selectedGroup = ref(null)
 
-const start_chartRef = ref(null)
-const commit_chartRef = ref(null)
-const end_chartRef = ref(null)
-const start_chart = ref(null)
-const commit_chart = ref(null)
-const end_chart = ref(null)
+const commit_chartRef = shallowRef(null)
+const end_chartRef = shallowRef(null)
+const commit_chart = shallowRef(null)
+const end_chart = shallowRef(null)
 
 const offsetData = ref({
-  start: {},
   commit: {},
   end: {},
 })
@@ -94,15 +87,12 @@ const selectNode = async (node) => {
   selectedTopics.value = []
   selectedGroup.value = null
 
-  start_chartRef.value = null
   commit_chartRef.value = null
   end_chartRef.value = null
-  start_chart.value = null
   commit_chart.value = null
   end_chart.value = null
 
   offsetData.value = {
-    start: {},
     commit: {},
     end: {},
   }
@@ -118,7 +108,7 @@ onMounted(async () => {
   await getData()
   initChart()
   // await fetchData()
-  timer = setInterval(fetchData, 2 * 60 * 1000) // 定时更新一次
+  timer = setInterval(fetchData, 5 * 60 * 1000) // 定时更新一次
   // EventsOn('resize', () => {
   //   start_chart.value?.resize()
   //   commit_chart.value?.resize()
@@ -133,9 +123,7 @@ const refreshTopic = async () => {
 
 // 初始化图表
 const initChart = () => {
-  if (start_chart.value) {
-    start_chart.value.dispose()
-  }
+
   echarts.use([
     TitleComponent,
     TooltipComponent,
@@ -146,7 +134,6 @@ const initChart = () => {
     UniversalTransition
   ]);
 
-  start_chart.value = echarts.init(start_chartRef.value, 'dark')
   const option = {
     title: {
       text: 'Kafka Offset监控',
@@ -157,22 +144,24 @@ const initChart = () => {
     xAxis: {
       type: 'category',
       boundaryGap: false,
+      data: []
     },
     yAxis: {
       type: 'value'
     },
+    legend: {
+      data: []
+    },
     series: []
   }
-  option.title.text='Start Offset监控'
-  start_chart.value.setOption(option)
 
   const commit_option = {...option}
-  commit_option.title.text='Committed Offset监控'
+  commit_option.title.text = '已提交 offset'
   commit_chart.value = echarts.init(commit_chartRef.value, 'dark')
   commit_chart.value.setOption({...commit_option})
 
   const end_option = {...option}
-  end_option.title.text='End Offset监控'
+  end_option.title.text = '终末 offset'
   end_chart.value = echarts.init(end_chartRef.value, 'dark')
   end_chart.value.setOption({...end_option})
 
@@ -180,40 +169,46 @@ const initChart = () => {
 
 // 更新图表数据
 const updateChart = () => {
-  if (!start_chart.value) return
 
-  const series = []
-  const legendData = []
+
   const chart_map = {
-    start: start_chart.value,
     commit: commit_chart.value,
     end: end_chart.value
   }
   for (const k in offsetData.value) {
-    console.log(k)
-    // start
-    Object.entries(offsetData.value[k]).forEach(([key, data]) => {
-      legendData.push(key)
+    let series = []
+    let legendData = []
+
+    // 迭代每个topic
+    let xs = []
+    Object.entries(offsetData.value[k]).forEach(([topic, data]) => {
+      // data是对象列表，item是time和offset
+      legendData.push(topic)
       series.push({
-        name: key,
+        name: topic,
         type: 'line',
         stack: 'Total',
         symbol: 'circle',
         symbolSize: 8,
-        data: data.map(item => [item.time, item.offset])
+        data: data.map(item => item.offset)
       })
+      xs = data.map(item => item.time)
     })
 
     chart_map[k].setOption({
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: xs
+      },
       legend: {
         data: legendData
       },
       series: series
     })
-    console.log(chart_map[k])
+    console.log(k, xs, legendData, series)
 
   }
-
 
 }
 
@@ -232,40 +227,29 @@ const fetchData = async () => {
       message.error(res.err)
     } else {
       const now = new Date()
-      const time =  now.getMonth() + "/"+ now.getDate() + " "+ now.getHours() + ":"+ now.getMinutes() + ":"+ now.getSeconds()
+      const time = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds()
 
       // 更新数据结构 start_map
       selectedTopics.value.forEach(topic => {
 
-        if (!offsetData.value.start[topic]) {
-          offsetData.value.start[topic] = []
-        }
-        offsetData.value.start[topic].push({
-          time,
-          offset: addOffsets(res.result.start_map[topic])|| 0
-        })
 
         if (!offsetData.value.commit[topic]) {
           offsetData.value.commit[topic] = []
         }
         offsetData.value.commit[topic].push({
-          time,
-          offset: addOffsets(res.result.commit_map[topic])|| 0
+          time: time,
+          offset: addOffsets(res.result.commit_map[topic]) || 0
         })
 
         if (!offsetData.value.end[topic]) {
           offsetData.value.end[topic] = []
         }
         offsetData.value.end[topic].push({
-          time,
-          offset: addOffsets(res.result.end_map[topic])|| 0
+          time: time,
+          offset: addOffsets(res.result.end_map[topic]) || 0
         })
 
 
-        // 只保留最近30个数据点
-        if (offsetData.value.start[topic].length > 30) {
-          offsetData.value.start[topic].shift()
-        }
         // 只保留最近30个数据点
         if (offsetData.value.commit[topic].length > 30) {
           offsetData.value.commit[topic].shift()
@@ -282,8 +266,8 @@ const fetchData = async () => {
     }
 
   } catch (e) {
-      message.error(e)
-  }finally {
+    message.error(e)
+  } finally {
     loading.value = false
   }
 }
@@ -295,7 +279,7 @@ const getData = async () => {
     const res = await GetTopics()
     const res2 = await GetGroups()
     if (res.err !== "" || res2.err !== "") {
-      message.error(res.err === res2.err? res.err : res.err + res2.err)
+      message.error(res.err === res2.err ? res.err : res.err + res2.err)
     } else {
       let topic_data_lst = []
       if (res.results) {
