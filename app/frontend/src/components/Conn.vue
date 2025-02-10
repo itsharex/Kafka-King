33143e91474aad58ab12d8249f8f9c4152b3d711
bronya0 <tangssst@qq.com>
2025-02-10 16:48:32 +0800

    Update Conn.vue

--- app/frontend/src/components/Conn.vue
+++ app/frontend/src/components/Conn.vue
@@ -80,7 +80,7 @@
           </n-form-item>
 
           <n-form-item :label="t('conn.skipTLSVerify')" path="skipTLSVerify">
-            <n-switch :round="false" checked-value="enable" unchecked-value="disable" v-model:value="currentNode.skipTLSVerify"/>
+            <n-switch :round="false" checked-value="true" unchecked-value="false" v-model:value="currentNode.skipTLSVerify"/>
           </n-form-item>
 
           <n-form-item label="TLS Cert File" path="tls_cert_file">
